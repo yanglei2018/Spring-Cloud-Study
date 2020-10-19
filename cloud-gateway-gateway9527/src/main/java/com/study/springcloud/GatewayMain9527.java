e94f6db4bc76daafb9dcb4394f66d129f06a21e6
lei yang <38425862+yanglei2018@users.noreply.github.com>
2020-10-19 22:18:04 +0800

    完成部分GateWay

---
+++ cloud-gateway-gateway9527/src/main/java/com/study/springcloud/GatewayMain9527.java
@@ -0,0 +1,19 @@
+package com.study.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Yang Lei
+ * @version 1.0
+ * @date 2020/10/19 22:05
+ * @description
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+}
